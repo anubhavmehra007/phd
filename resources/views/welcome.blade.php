1740ef01405545a5206a85daa12a67dc6e418dd8
hem bhatt <hembhatttku@gmail.com>
2019-02-01 12:38:16 +0530

    khela shuru

--- resources/views/welcome.blade.php
+++ resources/views/welcome.blade.php
@@ -81,7 +81,7 @@
 
             <div class="content">
                 <div class="title m-b-md">
-                    Laravel
+                    PHD Portal
                 </div>
 
                 <div class="links">
